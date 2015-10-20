2caa69bc77a1ba33a2392e2e0254d47c795efeed
gaglianr <gaglianr@onid.oregonstate.edu>
2015-10-19 20:15:58 -0700

    Fixed label size.

--- Widgets/Label.py
+++ Widgets/Label.py
@@ -5,6 +5,6 @@ from BaseWidget import BaseWidget
 """ Label """
 class Label(BaseWidget):
     def __init__(self, text, y, x):
-        BaseWidget.__init__(self, 3, len(text) + 5, y, x)
+        BaseWidget.__init__(self, 1, len(text) + 1, y, x)
         self.Win.addstr(text)
         self.Refresh()

99caf321283156b6bb52e67bba27b31311bbe805
gaglianr <gaglianr@onid.oregonstate.edu>
2015-10-22 20:38:59 -0700

    Updated Button to inherit from Label.  Updated Label to contain color reversal methods.

--- Widgets/Button.py
+++ Widgets/Button.py
@@ -1,27 +1,19 @@
 import curses
 import curses.panel
-from BaseWidget import BaseWidget
+#from BaseWidget import BaseWidget
+from Label import Label
 
 """ Button """
-class Button(BaseWidget):
+class Button(Label):
     def __init__(self, text, method, y, x):
-        BaseWidget.__init__(self, 1, len(text) + 5, y, x)
-        self.Text = "[ " + text + " ]"
+        text = "[ " + text + " ]"
+        Label.__init__(self, text, y, x)
         self.CallMethod = method
-        self.TextMode = curses.A_NORMAL
-        self.Refresh()
         
-    def DisplayText(self):
-        self.Win.addstr(0, 0, " " * (self.Characters - 1))
-        self.Win.addstr(0, 0, self.Text, self.TextMode)
-        self.Refresh()
-         
     def Active(self):
         selected = True
         # highlight current widget to show it is active
-        self.TextMode = curses.A_REVERSE
-        self.DisplayText()
-        self.Refresh()
+        self.Highlight()
         
         while selected:
             key = self.Win.getch()
@@ -31,14 +23,12 @@ class Button(BaseWidget):
                 self.CallMethod()
                 
                 # should I leave the deselecting of the widget for the screen handler object?
-                self.TextMode = curses.A_NORMAL
-                self.DisplayText()
+                self.UnHighlight()
                 selected = False
 
             elif key in [ord('\t'), 9]:
                 # stop highlighting current widget
-                self.TextMode = curses.A_NORMAL
-                self.DisplayText()
+                self.UnHighlight()
                 selected = False
                 # TODO: give notification to screen object that TAB was pressed (for selecting next widget)
                 #       potentially can use curses.ungetch(key) here
--- Widgets/Label.py
+++ Widgets/Label.py
@@ -6,5 +6,23 @@ from BaseWidget import BaseWidget
 class Label(BaseWidget):
     def __init__(self, text, y, x):
         BaseWidget.__init__(self, 1, len(text) + 1, y, x)
-        self.Win.addstr(text)
+        self.Text = text
+        self.TextMode = curses.A_NORMAL
+        self.UpdateDisplay()
+    
+    # Updates the display with the current text mode
+    def UpdateDisplay(self):
+        self.Win.erase()
+        self.Win.addstr(self.Text, self.TextMode)
         self.Refresh()
+    
+    # Highlights the text by reversing the foreground/background
+    def Highlight(self):
+        self.TextMode = curses.A_REVERSE
+        self.UpdateDisplay()
+    
+    # UnHighlights the text by setting foreground/background to current
+    # terminal colors
+    def UnHighlight(self):
+        self.TextMode = curses.A_NORMAL
+        self.UpdateDisplay()

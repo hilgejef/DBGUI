fffc660e2a3217e5ac296640a75b9cd6e981f24b
Jon Moore <jwmoore5024@gmail.com>
2015-10-26 23:18:29 -0700

    DataTable start

---
+++ Tests/datatable_tests.py
@@ -0,0 +1,19 @@
+import curses
+from BaseWidget import BaseWidget
+from DataTable import DataTable
+
+# These actions will be handled in the Global file
+stdscr = curses.initscr()
+curses.cbreak()
+curses.noecho()
+stdscr.keypad(1)
+
+# Test DataTable
+datatbl = DataTable(8, 30, 2, 2)
+datatbl.UpdateDisplay()
+
+# Clean up handled in Global file 
+curses.nocbreak()
+stdscr.keypad(0)
+curses.echo()
+curses.endwin()
---
+++ Widgets/DataTable.py
@@ -0,0 +1,84 @@
+#################################################################
+# Authors:          Rich Gagliano, Jonathan Moore, Jeffrey Hilger
+# Date Created:     10/26/2015
+# Date Modified:    10/26/2015
+# File Name:        DataTable.py
+#
+# Overview:
+#    TODO: Project overview
+#
+#################################################################
+
+import curses
+import curses.panel
+from BaseWidget import BaseWidget
+from Label import Label
+
+
+""" Data Table """
+class DataTable(BaseWidget):
+    def __init__(self, lines, characters, y, x):        # TODO:  add Results object in to be passed into init
+        BaseWidget.__init__(self, lines, characters, y, x)
+        # number of rows, cols needed by data table if screen size wasn't a limiting factor
+        self.PosY = 0
+        self.PosX = 0
+        
+        # later Rows and Columns will be determined by Results Object
+        # following values are arbitrary
+        self.Rows = 3
+        self.Columns = 3
+        self.ColWidth = 8
+        self.RowHeight = 1
+        self.TotalY = self.Rows * self.RowHeight + 3   # extra rows for labels, border, and scroll bar
+        self.TotalX = self.Columns * (self.ColWidth + 1) + 4  # extra rows for row #, border, and scroll bar
+        self.Pad = curses.newpad(self.TotalY + 1, self.TotalX + 1)
+        
+        #
+        # ARBITRARY DATA FOR TESTING
+        # Data in the application will come from a Results Object
+        #
+        
+        # create labels for cells
+        self.ColLabels = [Label("tstCol" + str(j), self.Y + 0, self.X + j * (self.ColWidth + 1) + 2) for j in range(self.Columns)]
+        for lbl in self.ColLabels:
+            lbl.Hide()
+            lbl.Refresh()
+        
+        # create data cells that table will be composed of
+        # [y, x] for positioning of data cells
+        self.DataCells = [[Label("tCell" + str(i) + str(j), self.Y + i + 2, self.X + j * (self.ColWidth + 1) + 2) for i in range(self.Rows)] for j in range(self.Columns)]
+        for y in range(self.Rows):
+            for x in range(self.Columns):
+                self.DataCells[y][x].Hide()
+                self.DataCells[y][x].Refresh()
+
+    def UpdatePad(self):
+        # self.Pad contains the entire graphical output of the DataTable
+        
+        # clear pad
+        self.Pad.erase()
+        
+        # Line 0: column labels
+        text = " |"
+        for l in self.ColLabels:
+            text += l.Text
+            text += "|"
+        self.Pad.addstr(0, 0, text)
+        
+        # Line 1: border
+        self.Pad.addstr(1, 0, "-" * self.TotalX)
+        
+        # Lines 2 to Rows + 2: iterate through rows
+        for y in range(self.Rows):
+            # DOES NOT ACCOUNT FOR TWO-DIGIT NUMBERS
+            text = str(y) + "|"
+            for x in range(self.Columns):
+                text += self.DataCells[y][x].Text
+                text += "|"
+            self.Pad.addstr(2 + y, 0, text)
+
+    def UpdateDisplay(self):
+        self.UpdatePad()
+        self.Pad.overlay(self.Win, self.PosY, self.PosX, self.Y, self.X, self.Lines - 1, self.Characters - 1)
+        #self.Pad.overlay(self.Win)
+        self.Refresh()

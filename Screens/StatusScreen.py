3daa24e2e5e43600786fd81d35ec246f55273d19
Jon Moore <jwmoore5024@gmail.com>
2015-11-23 13:23:49 -0800

    Basic Scrolling added

--- Screens/StatusScreen.py
+++ Screens/StatusScreen.py
@@ -9,7 +9,7 @@
 #
 ###############################################################################
 
-import curses
+import curses, sys
 from CDBCore import CDBCore
 from BaseWidget import BaseWidget
 from Label import Label
@@ -26,12 +26,16 @@ class StatusScreen(BaseScreen):
         # screen settings & spacing variables
         self.ScreenStartY = 20
         self.ScreenHeight = 4       # Status Screen will be 4 tiles high including it's border
-        self.TotalLabelsInScreen = 2
+        self.TotalLabelsInScreen = 2  # number of logs displayed in screen at a time
         
         self.logLabelY = self.ScreenStartY + 1
         self.logLabelX = 30
         genLabelY = self.ScreenStartY + 1
         genLabelX = 1
+        
+        self.LogDisplayPos = 0      # index of self.Log to be displayed on bottom line of screen
+        self.CursorPos = 0          # index of self.Log that the cursor is currently on
+        self.CursorActive = False
         
         # blank widget for drawing a border around the entirety of the Status Screen
         self.PassiveWidgets.append(BaseWidget(self.ScreenHeight,
@@ -42,7 +46,7 @@ class StatusScreen(BaseScreen):
         self.PassiveWidgets[0].ToBottom()
         
         # label widget for persistent messages
-        self.PassiveWidgets.append(Label("Ctrl-M for Main Menu", genLabelY, genLabelX))
+        self.PassiveWidgets.append(Label("Ctrl-T for Main Menu", genLabelY, genLabelX))
         self.PassiveWidgets[1].ToTop()
         
         # label widgets for logged system messages
@@ -69,10 +73,19 @@ class StatusScreen(BaseScreen):
         if not self.Log:
             return
         
-        lblIdx = self.TotalLabelsInScreen + 1
+        lblIdx = self.TotalLabelsInScreen + 1   # index of first PassiveWidgets to display from top to bottom
         for msgIdx in range(min(self.TotalLabelsInScreen, len(self.Log))):
-            lblMsg = self.Log[msgIdx][:CDBCore.TERMINAL_CHARACTERS - self.logLabelX - 2]
+            # x width available for label messages
+            charsAvailable = CDBCore.TERMINAL_CHARACTERS - self.logLabelX - 2
+            lblMsg = self.Log[msgIdx + self.LogDisplayPos][:charsAvailable]
+            fillerChars = charsAvailable - len(lblMsg)
+            lblMsg += (" " * fillerChars)
+            # clear and redraw the label
+            self.PassiveWidgets[lblIdx].Win.erase()
             self.PassiveWidgets[lblIdx] = Label(lblMsg, self.logLabelY + lblIdx - 2, self.logLabelX)
+            # HIGHLIGHT TEXT if the label is the current cursor position, and messages are being scrolled
+            if self.CursorActive and self.CursorPos == msgIdx + self.LogDisplayPos:
+                self.PassiveWidgets[lblIdx].Highlight()
             self.PassiveWidgets[lblIdx].ToTop()
             lblIdx -= 1
         self.Show()
@@ -82,3 +95,42 @@ class StatusScreen(BaseScreen):
         self.PassiveWidgets[1] = Label(msgString[:self.logLabelX - 4], self.logLabelY, self.logLabelX)
         self.PassiveWidgets[1].ToTop()
         self.Show()
+        
+    def MessageScroll(self):
+        self.CursorActive = True
+        self.UpdateLogLabels()
+        
+        while self.CursorActive:
+            currentLabelIndex = 1 + self.TotalLabelsInScreen - (self.CursorPos - self.LogDisplayPos)
+            try:
+                key = self.PassiveWidgets[currentLabelIndex].Win.getch()
+            except:
+                print "currentLabelIndex = ", currentLabelIndex
+                print "self.CursorPos = ", self.CursorPos
+                print "self.LogDisplayPos = ", self.LogDisplayPos
+                sys.exit()
+            
+            if key in [curses.KEY_DOWN, ord('s')]:
+                if self.CursorPos > 0:
+                    self.CursorPos -= 1
+                    if self.LogDisplayPos > self.CursorPos:
+                        self.LogDisplayPos = self.CursorPos
+                self.UpdateLogLabels()
+            elif key in [curses.KEY_UP, ord('w')]:
+                if self.CursorPos < (len(self.Log) - 1):
+                    self.CursorPos += 1
+                    if self.LogDisplayPos < self.CursorPos - self.TotalLabelsInScreen + 1:
+                        self.LogDisplayPos += 1
+                self.UpdateLogLabels()
+            elif key in [ord('\t'), 9]:     # TAB
+                # exit for screen
+                self.CursorPos = 0
+                self.CursorActive = False
+                self.UpdateLogLabels()
+                return
+            elif key in [ord('\n'), 10]:    # ENTER
+                #TODO: When a message is selected it should call a popup screen that displays the full message
+                pass
+        
+        
+        
--- Tests/StatusScreenTest.py
+++ Tests/StatusScreenTest.py
@@ -1,3 +1,5 @@
+import curses
+
 from CDBCore import CDBCore
 from StatusScreen import StatusScreen
 
@@ -6,4 +8,6 @@ statusScreen = StatusScreen()
 statusScreen.AddStatusMessage("Test Message 1")
 statusScreen.AddStatusMessage("Test Message 2.  This one is supposed to go really far across the screen")
 statusScreen.AddStatusMessage("Test Msg 3. Does scrolling work?")
+statusScreen.AddStatusMessage("More messages")
+statusScreen.MessageScroll()
 CDBCore.CleanupCurses()

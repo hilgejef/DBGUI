7b4690c97d4383ec2de5021d936780701033924c
gaglianr <gaglianr@onid.oregonstate.edu>
2015-12-10 18:55:07 -0700

    Cleaned up BaseConnection.

--- ConnectionObjects/BaseConnection.py
+++ ConnectionObjects/BaseConnection.py
@@ -1,8 +1,5 @@
 ###############################################################################
-# Author:		Rich Gagliano
-# Date Created:		11/1/2015
-# Date Modified:	11/1/2015
-# File Name:		BaseConnection.py
+# BaseConnection
 #
 # Overview:
 #	Provides the base connection framework for specific DB connection
@@ -14,9 +11,12 @@
 #		- Host
 #		- Port
 #		- Database
+#		- DBType
 #
 #
 ###############################################################################
+
+from ResultStatus import ResultStatus
 
 class BaseConnection:
     def __init__(self, user, password, host, port, database, dbtype=""):
@@ -30,17 +30,17 @@ class BaseConnection:
     
     # Opens a connection to the DB server. To be overloaded in children.  
     def Connect(self):
-        return False # TODO: Update with result status object as return value
+        return ResultStatus(False, "Unimplemented base function")
     
     # Allows for query execution from a string.To be overloaded in children.
-    # WARNING: Open to SQL injection.  Never use with user input.
+    # WARNING: Open to SQL injection.  Use with care.
     def QueryString(self, query):
-        return False # TODO: Update with result status object as return value
+        return ResultStatus(False, "Unimplemented base function")
     
     # Allows for buffered query execution. To be overloaded in children.
     # SQL injection safe.
     def QueryBuffered(self, query, values):
-        return False # TODO: Update with result status object as return value
+        return ResultStatus(False, "Unimplemented base function")
     
     # Parses the results of a query into a format that can be used by the 
     # DataTable widget.

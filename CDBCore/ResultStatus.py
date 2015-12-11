31d31e96453ea88a500b9d3f19c8ee97535234b3
hilgejef <hilgejef@oregonstate.edu>
2015-12-10 19:02:28 -0800

    Deleted CDBCore backup, added Core headers

--- CDBCore/ResultStatus.py
+++ CDBCore/ResultStatus.py
@@ -1,17 +1,8 @@
 ###############################################################################
-# Author:		Rich Gagliano
-# Date Created:		11/5/2015
-# Date Modified:	11/5/2015
-# File Name:		ResultStatus.py
+# ResultStatus
 #
-# Overview:
-#	Provides a standardized framework for passing results within the
-#	application.  Data includes:
-#
-#		Success: Boolean value indicating success or failure
-#		Message: The error message if failure.
-#		Data:	 Any data that the result contains.
-#
+# Object containing query result data			
+#	
 ###############################################################################
 
 class ResultStatus():
